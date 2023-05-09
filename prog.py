a7d8b3674fe821516be9b7624494dad27678b682
ilyx666 <113046049+ilyx666@users.noreply.github.com>
2023-05-09 20:46:12 +0700

    Add files via upload

--- prog.py
+++ prog.py
@@ -1,28 +1,91 @@
-import tkinter as tk
-from tkinter import Text
+import tkinter
+import tkinter.messagebox
+import customtkinter
 
-def button_click():
-    value1 = entry1.get()
-    value2 = entry2.get("1.0", "end-1c")
-    print("Количество фото:", value1)
-    print("Отзыв:", value2)
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
 
-root = tk.Tk()
-root.geometry("900x600")
 
-label1 = tk.Label(root, text="Количество фото:")
-label1.pack()
 
-entry1 = tk.Entry(root)
-entry1.pack()
 
-label2 = tk.Label(root, text="Отзыв:")
-label2.pack()
+class App(customtkinter.CTk):
+    def __init__(self):
 
-entry2 = tk.Text(root, width=50, height=10)
-entry2.pack()
+        super().__init__()
+        def button_event(self):
+            print(self.textbox.get("0.0", "end"))
 
-button = tk.Button(root, text="ГОТОВО!", command=button_click)
-button.pack()
+        # configure window
+        self.title("AuthenticFeedback")
+        self.geometry(f"{1100}x{580}")
 
-root.mainloop()
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="AuthenticFeedback", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+
+
+
+        # create textbox
+        self.textbox = customtkinter.CTkTextbox(self, width=700)
+        self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0))
+
+        self.entry = customtkinter.CTkEntry(self, width=40)
+        self.entry.place(relx=0.46, rely=0.52, anchor=tkinter.CENTER)
+
+        self.label2 = customtkinter.CTkLabel(self)
+        self.label2 = customtkinter.CTkLabel(self, text="Кол-во прикрепленных фото:", anchor="w")
+        self.label2.place(relx=0.355, rely=0.52, anchor=tkinter.CENTER)
+
+        self.label = customtkinter.CTkLabel(self)
+        self.label = customtkinter.CTkLabel(self, text="Отзыв:", anchor="w", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.label.place(relx=0.31, rely=0.1, anchor=tkinter.CENTER)
+
+
+        self.but = customtkinter.CTkButton(self, text='Готово', command=button_event(self))
+        self.but.place(relx=0.847, rely=0.52, anchor=tkinter.CENTER)
+
+
+       
+
+
+
+        # set default values
+        self.appearance_mode_optionemenu.set("Dark")
+        self.scaling_optionemenu.set("100%")
+
+
+
+
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+
+
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
